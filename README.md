1d925ed2eead68f8fb326fe280c2a9b6f09e7407
Eyal Lior <eyallior@eyallior.com>
2024-12-22 19:25:44 +0200

    progress

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Vanilla Boot
 
-Projects' guiding principles:
+## Minimal Framework ꞏ Maximum Control ꞏ Pure Javascript ꞏ Pure Freedom
+
+### Project's guiding principles:
 
 * Non pervasive - You can do anything you could do in Vanilla
 * Download only used modules (or decide which modules to preload)
@@ -13,3 +15,11 @@ Projects' guiding principles:
 * No compilation (easy to reverse engineer starting from browser)
 * No chunking to incoherent little code pieces
 * Due to no framework as base and fewer dependabilities, we get long term reliability (no framework change will cause this project to be unusable)
+
+### Features:
+
+* Ability to create modules in completely different strategy for each
+* Ability to embed modules in parent modules and to communicate with other modules
+* Open multiple instances of the same module
+* Dynamic loading of module's Javascript, css or any kind of dependencies
+* 
---
+++ demodata/getMenu.js
@@ -0,0 +1,45 @@
+{
+    "status": "success",
+    "menuitems": [
+        {
+            "title": "Non working Test 2",
+            "submenu": [
+                {
+                    "title": "Non working Test Service 11",
+                    "action": "module",
+                    "val": "TestService11"
+                },
+                {
+                    "title": "Non working Test Service 12",
+                    "action": "module",
+                    "val": "TestService12"
+                }
+            ]
+        },
+        {
+            "title": "Test",
+            "submenu": [
+                {
+                    "title": "Non working Test Service 13",
+                    "action": "module",
+                    "val": "TestService11"
+                },
+                {
+                    "title": "Test Screen 1",
+                    "action": "module",
+                    "val": "TestEmptyService"
+                }
+            ]
+        },
+        {
+            "title": "Admin",
+            "action": "module",
+            "val": "ServerAdminService"
+        },
+        {
+            "title": "Setup",
+            "action": "module",
+            "val": "SetupScreen"
+        }
+    ]
+}
